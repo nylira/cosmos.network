7cbbb24d4a68f3e6357a991054fb18bb1fa46386
Peng Zhong <peng@nylira.com>
2017-10-05 21:18:48 +0800

    update routes

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
 
   { path: '/hackatom', name: 'hackatom-en-us', component: r('HackAtom') },
   { path: '/hackatom/zh-cn', name: 'hackatom-zh-cn', component: r('HackAtomZhCn') },
-  { path: '/validators', name: 'validators', component: r('Validators') },
+  // { path: '/validators', name: 'validators', component: r('Validators') },
 
   // redirects
   { path: '/whitepaper/en-US', redirect: '/whitepaper' },
