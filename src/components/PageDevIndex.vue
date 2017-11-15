fc78a67b26a954afb3800ab7a174f93e8f2e099b
Peng Zhong <peng@nylira.com>
2017-11-15 09:26:57 -0600

    update Dev page

--- src/components/PageDevIndex.vue
+++ src/components/PageDevIndex.vue
@@ -10,16 +10,20 @@
 
   part(title='  '): text-container
     h2 Documentation
-    p Text.
+    p All of the documentation for Cosmos SDK can be found here.
+    btn(type="anchor" href="https://cosmos.read-the-docs.io"
+      target="_blank" value="Documentation")
 </template>
 
 <script>
 import {mapGetters} from 'vuex'
 import Part from './common/NiPart'
 import TextContainer from './common/NiTextContainer'
+import Btn from '@nylira/vue-button'
 export default {
   name: 'page-dev-index',
   components: {
+    Btn,
     Part,
     TextContainer
   },
