753e7c9726da208067cc9b5a2cc02a666127270c
Peng Zhong <peng@nylira.com>
2017-12-01 12:31:22 +0900

    update validators index page

--- src/components/pages/PageValidatorsFaq.vue
+++ src/components/pages/PageValidatorsFaq.vue
@@ -4,23 +4,15 @@ page(title="Validator FAQ" subtitle="Frequently asked questions about Cosmos Val
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import BodyText from 'content/en-US/validators-faq.md'
-import Btn from '@nylira/vue-button'
 import Page from 'common/NiPage'
-import Part from 'common/NiPart'
 import TextContainer from 'common/NiTextContainer'
 export default {
-  name: 'page-validators',
+  name: 'page-validators-faq',
   components: {
     BodyText,
-    Btn,
     Page,
-    Part,
     TextContainer
-  },
-  computed: {
-    ...mapGetters(['validators', 'links'])
   },
   mounted () {
     document.title = 'Validators FAQ - Cosmos - Internet of Blockchains'
