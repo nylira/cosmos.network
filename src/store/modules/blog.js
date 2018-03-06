35a8ab554acb00f179f64e6c4dbacc17e0505dba
Peng Zhong <peng@nylira.com>
2018-03-06 11:52:52 +0800

    use own cors proxy

--- src/store/modules/blog.js
+++ src/store/modules/blog.js
@@ -9,7 +9,7 @@ const mutations = {
     const parser = new Parser()
     const RSS_URL = 'https://medium.com/feed/cosmos-blockchain'
     // TODO: Replace demo cors proxy with our own proxy
-    const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
+    const PROXY_URL = 'https://cors.testnets.interblock.io/'
     const FULL_URL = PROXY_URL + RSS_URL
 
     let feed = await parser.parseURL(FULL_URL)
