25c8dd4540d00df708facab59e3d8bb7267461b2
Peng Zhong <peng@nylira.com>
2018-03-06 20:59:32 +0800

    update proxy url comment

--- src/store/modules/blog.js
+++ src/store/modules/blog.js
@@ -8,7 +8,6 @@ const mutations = {
   async initializeBlog (state) {
     const parser = new Parser()
     const RSS_URL = 'https://medium.com/feed/cosmos-blockchain'
-    // TODO: Replace demo cors proxy with our own proxy
     const PROXY_URL = 'https://cors.testnets.interblock.io/'
     const FULL_URL = PROXY_URL + RSS_URL
 
