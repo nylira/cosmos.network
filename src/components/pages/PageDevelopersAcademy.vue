3012a7ad01aa085201d59d155b578e26666fa56e
Peng Zhong <peng@nylira.com>
2018-03-29 20:15:32 +0200

    add link

--- src/components/pages/PageDevelopersAcademy.vue
+++ src/components/pages/PageDevelopersAcademy.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
 page(title="Cosmos Academy" subtitle="Participate in the Academy to learn how to build your project on the Cosmos Network.")
+  div(slot="menu")
+    btn(icon="code" value="Academy on GitHub" type="anchor" href="https://github.com/cosmos/cosmos-academy" target="_blank" color="primary")
   text-container(url='https://api.github.com/repos/tendermint/aib-data/contents/md/cosmos-academy.md')
 </template>
 
 <script>
+import Btn from '@nylira/vue-button'
 import Page from 'common/NiPage'
 import TextContainer from 'common/NiTextContainer'
 export default {
   name: 'page-academy',
   metaInfo: { title: 'Academy' },
   components: {
+    Btn,
     Page,
     TextContainer
   }
