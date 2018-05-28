cafe8977e761a3dda98ea4535b04d66ef064fd4a
Peng Zhong <172531+nylira@users.noreply.github.com>
2018-05-28 14:11:42 +0800

    shrink image sizes with imageoptim, add devcorn

--- src/components/pages/PageAssets.vue
+++ src/components/pages/PageAssets.vue
@@ -91,6 +91,13 @@ export default {
         date: "2018-01-03",
         url:
           "https://drive.google.com/open?id=1s1gXL2AJpGyeY1i-33bdMT3cis_eWTg9"
+      },
+      {
+        id: "cosmos-devcorn",
+        title: "Cosmos DevCorn",
+        date: "2017-10-14",
+        url:
+          "https://drive.google.com/open?id=0Bz0mZENfm-j8cDlSdDJ0VlVEWkE"
       }
     ]
   })
