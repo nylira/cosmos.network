2cc02fdc7b9152f626ceb022ef07230b95a7afaf
Peng Zhong <172531+nylira@users.noreply.github.com>
2018-06-13 13:40:40 +0800

    fix scrolling to anchor in markdown text

--- src/components/common/NiMdText.vue
+++ src/components/common/NiMdText.vue
@@ -4,8 +4,10 @@
 
 <script>
 import { Base64 } from "js-base64"
-import MarkdownIt from "markdown-it"
 import axios from "axios"
+import MarkdownIt from "markdown-it"
+import MarkdownItAnchor from "markdown-it-anchor"
+import scrollTo from "scroll-to"
 export default {
   name: "ni-md-text",
   data: () => ({
@@ -17,13 +19,34 @@ export default {
         html: true,
         linkify: true,
         typographer: true
-      })
+      }).use(MarkdownItAnchor)
       return md.render(text)
+    },
+    scrollToIt(e) {
+      e.preventDefault()
+
+      let scrollSpeed = 666
+      let el = document.querySelector(e.target.getAttribute("href"))
+      let y
+
+      if (el.id === "#page-top") {
+        y = 0
+      } else {
+        y = el.offsetTop - 48 - 16
+      }
+
+      scrollTo(0, y, { duration: scrollSpeed })
     }
   },
   async mounted() {
     let data = (await axios.get(this.url)).data
     this.text = Base64.decode(data.content)
+
+    await this.$nextTick()
+    let inlineLinks = this.$el.querySelectorAll("a")
+    Array.from(inlineLinks).forEach(link => {
+      link.addEventListener("click", event => this.scrollToIt(event))
+    })
   },
   props: ["url"]
 }
