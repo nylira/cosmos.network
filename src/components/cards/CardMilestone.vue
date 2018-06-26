8013d58edb9585e854074cc5bb426d8e5bab3b9b
Peng Zhong <172531+nylira@users.noreply.github.com>
2018-06-26 14:53:18 +0800

    hide completed milestone details

--- src/components/cards/CardMilestone.vue
+++ src/components/cards/CardMilestone.vue
@@ -1,13 +1,19 @@
 <template lang="pug">
 .ni-milestone(:class="cardClass" :style="cardStyle")
   .ni-milestone__container(:id="milestone.id")
-    .ni-milestone__main
-      .ni-milestone__header
-        .ni-milestone__icon
-          i.material-icons.done(v-if="milestone.date") done
-          i.material-icons(v-else) timelapse
-        .ni-milestone__title {{ milestone.title }}
-      .ni-milestone__value {{ milestone.description }}
+    .ni-milestone__header(v-if="milestone.date" @click="toggleDetails")
+      .ni-milestone__icon
+        i.material-icons.done done
+      .ni-milestone__title {{ milestone.title }}
+      .ni-milestone__action
+        i.material-icons(v-if="!details") help_outline
+        i.material-icons(v-else) close
+    .ni-milestone__header(v-else)
+      .ni-milestone__icon
+        i.material-icons hourglass_empty
+      .ni-milestone__title {{ milestone.title }}
+    .ni-milestone__details(v-if="details")
+      .ni-milestone__description {{ milestone.description }}
       .ni-milestone__footer(v-if="milestone.date")
         .ni-milestone__date {{ milestone.date }}
         .ni-milestone__network {{ milestone.network }}
@@ -35,6 +41,19 @@ export default {
       }
     }
   },
+  data: () => ({
+    details: true
+  }),
+  mounted() {
+    if (this.milestone.date) {
+      this.details = false
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.details = !this.details
+    }
+  },
   props: ["milestone", "type"]
 }
 </script>
@@ -45,27 +64,27 @@ export default {
 .ni-milestone
   margin 1rem
   &.ni-milestone--done
-    .ni-milestone__main
-      color var(--bright)
-      background var(--app-fg)
-      border-color var(--link)
+    .ni-milestone__header
+      cursor pointer
+    &:hover
+      .ni-milestone__header
+        background var(--app-fg)
+      .ni-milestone__action i.material-icons
+        color var(--hover)
 
 .ni-milestone__container
-  width 100%
-
-.ni-milestone__main
   max-width 40rem
-  border 2*px solid var(--bc)
-  background var(--app-bg)
+  border 1px solid var(--bc)
+  background var(--app-fg)
 
 .ni-milestone__header
   height 2rem
   display flex
   align-items center
-  border-bottom 1px solid var(--bc)
+  justify-content space-between
+  background var(--app-bg)
 
 .ni-milestone__icon
-
   i.material-icons
     width 2rem
     height 2rem
@@ -73,16 +92,33 @@ export default {
     align-items center
     justify-content center
     color var(--dim)
+    border-right 1px solid var(--bc)
+    color var(--bright)
+    background var(--bc)
   i.done
     color var(--bright)
     background var(--link)
+    border-right none
 
 .ni-milestone__title
+  flex 1
   padding 0 0.5rem
   color var(--bright)
   font-weight 500
 
-.ni-milestone__value
+.ni-milestone__action
+  i.material-icons
+    width 2rem
+    height 2rem
+    display flex
+    align-items center
+    justify-content center
+    color var(--dim)
+
+.ni-milestone__header + .ni-milestone__details
+  border-top 1px solid var(--bc)
+
+.ni-milestone__description
   color var(--txt)
   padding 0.5rem 0.75rem
   font-size sm
@@ -94,7 +130,6 @@ export default {
   display flex
   align-items center
   justify-content space-between
-  border-bottom 1px solid var(--bc)
 
   font-size sm
   color var(--dim)
@@ -108,5 +143,4 @@ export default {
     font-size 1.25rem
     font-weight 400
     line-height 3rem
-
 </style>
